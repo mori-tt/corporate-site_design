b31c420eed22860caedbb6c92b7cc2ee43659a00
mori-tt <git@stackblitz.com>
2025-04-01 04:10:36 +0900

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body className="antialiased min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow pt-16">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
